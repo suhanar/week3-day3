44b760b5d920ae599e52965baf2d462fbc85d5e9
unknown <suhanasiraj60@gmail.com>
2022-12-07 16:30:29 -0800

    new commit

--- week4-day1-hw.js
+++ week4-day1-hw.js
@@ -1,8 +1,8 @@
 class Hamster{
-    constructor(name='suhu'){
-        this.owner = '';
+    constructor(name='suhu',price = 15,owner =''){
+        this.owner = owner;
         this.name = name;
-        this.price = 15;
+        this.price = price;
     }
     wheelRun(){
         console.log("squeek squeek");
@@ -19,15 +19,16 @@ const hamster1 = new Hamster('rayu');
 // console.log(hamster1.eatFood());
 // console.log(hamster1.getPrice());
 
-class Person{
-    constructor(name='suhu'){
+class Person extends Hamster{
+    constructor(name='suhu',price,owner,age = 0,height = 0,weight=0,mood=0,hamsters=[],bankAccount=0){
+        super(name,price,owner);
         this.name = name;
-        this.age  = 0;
-        this.height = 0;
-        this.weight = 0;
-        this.mood = 0;
+        this.age  = age;
+        this.height = height;
+        this.weight = weight;
+        this.mood = mood;
         this.hamsters = [];
-        this.bankAccount = 0;
+        this.bankAccount = bankAccount;
     }
     getName(){
         return this.name;
@@ -51,18 +52,17 @@ class Person{
     }
     ageUp(){
         return(
-            `${this.age++},${this.height++},${this.weight++},${this.mood--},${this.bankAccount += 10}`);
+            `${this.age++},${this.height++},${this.weight++},${this.mood--},${this.bankAccount +=10}`);
       
 
     }
-    getPrice(){
-        return this.bankAccount--;
-    }
+    
     buyHamster(hamster){
         //console.log(this.mood+=10);
         //console.log(this.getPrice());
-        console.log(this.bankAccount);
-        return (`${this.hamsters.push(hamster)},${this.mood+=10},${this.getPrice()-1}`);
+        //console.log(this.bankAccount);
+        //console.log(this.hamsters.push(hamster));
+        return (`${this.hamsters.push(hamster)},${this.mood+=10},${this.bankAccount -  this.price}`);
        
         
     }
@@ -71,22 +71,37 @@ class Person{
 const Timmy = new Person();
 Timmy.age = 5;
 Timmy.weight = 5;
+
+Timmy.eat();
+Timmy.eat();
+Timmy.eat();
+Timmy.eat();
+Timmy.eat();
 //Timmy.age = 9;
 
 //console.log(Timmy.age);
-console.log("Eat 5 times" +Timmy.eat());
-console.log(Timmy.exercise());
-console.log(Timmy.ageUp());
-console.log(Timmy.buyHamster('hamster1'));
+console.log("Eat 5 times " +Timmy.eat());
+Timmy.exercise();
+Timmy.exercise();
+Timmy.exercise();
+Timmy.exercise();
+Timmy.exercise();
+console.log("Losing weight by excercise "+Timmy.exercise());
+//console.log(Timmy.ageUp());
+console.log("Price of Hamster "+Timmy.buyHamster('hamster1'));
 Timmy.hamsters.push('Gus');
 console.log(Timmy.hamsters);
-
+Timmy.age = 9;
 const Gus = new Hamster();
 Gus.owner = 'Timmy';
+//console.log('Timmy eat '+ Timmy.eat());
 Timmy.age = 15;
-Timmy.weight *= 2;
+Timmy.eat();
+Timmy.eat();
+Timmy.exercise();
+Timmy.exercise();
 //console.log(Timmy.exercise());
-console.log(Timmy.exercise()*2);
+console.log("Timmy's weight "+Timmy.exercise());
 
 
 class Dinner{
