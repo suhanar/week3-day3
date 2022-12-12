5c69a8ae2365709315ba228aa76cad37b67db84b
unknown <suhanasiraj60@gmail.com>
2022-12-12 06:18:59 -0800

    Get Even answered

--- week4-day3.js
+++ week4-day3.js
@@ -2,3 +2,11 @@
 for(let i =0;i<=20;i++){
     console.log(i)
 }
+
+//Write a for loop that will log only the even numbers in 0 through 200.
+for(let i=0;i<=200;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+    
+}
