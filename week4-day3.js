1714af5e0b70efe6f294e43b1fa7a5cff115a2a0
unknown <suhanasiraj60@gmail.com>
2022-12-12 06:35:12 -0800

    Wild Wild Life answered

--- week4-day3.js
+++ week4-day3.js
@@ -1,17 +1,36 @@
 //Write a for loop that will log the numbers 1 through 20.
 for(let i =0;i<=20;i++){
-    console.log(i)
+   // console.log(i)
 }
 
 //Write a for loop that will log only the even numbers in 0 through 200.
 for(let i=0;i<=200;i++){
     if(i%2==0){
-        console.log(i);
+        //console.log(i);
     }
     
 }
 
 //Fizz Buzz
 for(let i=0;i<=20;i++){
-    console.log((i%3==0&&i%5==0) ? "FizzBuzz" : (i%3==0)? "Fizz" : (i%5==0)? "Buzz" : i);
+    //console.log((i%3==0&&i%5==0) ? "FizzBuzz" : (i%3==0)? "Fizz" : (i%5==0)? "Buzz" : i);
 }
+
+//Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2]++;
+
+wolfy[3]='Gotham City';
+console.log(wolfy);
+const arr=[dart[3]];
+arr.push('Hawkins');
+dart[3]=arr;
+console.log(dart);
+wolfy.shift();
+wolfy.unshift('GameBoy');
+console.log(wolfy);
